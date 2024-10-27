0b0e7e5b4ed6a38267521b794b744e70b29ebbc5
Maik Emanoel <maikemanoel11@gmail.com>
2024-10-26 21:09:45 -0300

    reverse-string completed

---
+++ src/exercises/reverse-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+function reverseString(s) {
+  return s.reverse()
+}
+
+console.log(reverseString(['h', 'e', 'l', 'l', 'o']))
+
+// Data: 20/10/2024
